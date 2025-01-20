07b35702dff5e3c9ee4c22450a0d72a6731b71c3
Youchunmaru <samuel.groener@gmx.de>
2025-01-20 12:41:38 +0100

    - add mysql connection and add example web page

---
+++ src/main/java/de/rbsulm/rbs_lf12/model/User.java
@@ -0,0 +1,22 @@
+package de.rbsulm.rbs_lf12.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
+
+  private String name;
+
+  private String email;
+
+}

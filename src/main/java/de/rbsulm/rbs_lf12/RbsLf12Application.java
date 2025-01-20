a343b6afa212380dab9dbd84ab257bcc68b0f463
Youchunmaru <samuel.groener@gmx.de>
2025-01-20 11:48:29 +0100

    inital

---
+++ src/main/java/de/rbsulm/rbs_lf12/RbsLf12Application.java
@@ -0,0 +1,13 @@
+package de.rbsulm.rbs_lf12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbsLf12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbsLf12Application.class, args);
+    }
+
+}
---
+++ src/test/java/de/rbsulm/rbs_lf12/RbsLf12ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rbsulm.rbs_lf12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbsLf12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
